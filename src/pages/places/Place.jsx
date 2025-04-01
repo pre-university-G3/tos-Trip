35af8b77622e552fb383cfaf36a1c0daeda5f91c
reanchanrin12 <ooppa5396@gmail.com>
2025-04-01 23:35:22 +0700

    place page fetch api

--- src/pages/places/Place.jsx
+++ src/pages/places/Place.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react';
+import getData from '../../services/get/getData';
 
 const Place = () => {
+   const [places, setPlaces] = useState([]);
+   const [category, setCategory] = useState([]);
+  
+    useEffect(() => {
+      async function getCategories() {
+        const data = await getData("places"); 
+        const categories = await getData("categories");
+        console.log("Fetched Data:", data); 
+        console.log("Fetched Data:",categories ); 
+        
+        if (Array.isArray(data)) {
+          // setPlaces(data); 
+          setCategory(categories);
+        } else {
+          console.error("Error: API response is not an array", data);
+        }
+      }
+      getCategories();
+    }, []);
+  
   return (
-    <div>Place</div>
+    <div>
+      {
+        // category.map((item, index) => {
+        //   <div key={index}>
+        //     <h1>{item.name}</h1>
+        //     <p>{item.category}</p>
+        //   </div>
+        // })
+
+        category.map((item, index) => {
+          return (
+            <div className='text-zinc-950' key={index}>
+              <h1 className='text-xl font-bold tex-rud-500'>{item.name}</h1>
+              <p className='text-pink-300'>{item.description}</p>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
