8016095e77918e34cf548312d0919837ec3ad009
Dita Rector <ditarector@MacBooks-Laptop.local>
2025-04-03 00:55:29 +0700

    detail page 85% done

--- src/pages/places/Place.jsx
+++ src/pages/places/Place.jsx
@@ -14,7 +14,7 @@ const Place = () => {
         console.log("Fetched Data:",categories ); 
         
         if (Array.isArray(data)) {
-          // setPlaces(data); 
+          setPlaces(data); 
           setCategory(categories);
         } else {
           console.error("Error: API response is not an array", data);
@@ -25,23 +25,40 @@ const Place = () => {
   
   return (
     <div>
-      {
-        // category.map((item, index) => {
-        //   <div key={index}>
-        //     <h1>{item.name}</h1>
-        //     <p>{item.category}</p>
-        //   </div>
-        // })
+      {places.length > 0 ? (
+        <ul>
+          {places.map((place) => (
+            <li key={place.id}>
+              <h2 className='text-black font-extrabold text-5xl mb-6 ml-3.5 mt-10'>{place.name}</h2>
+              <hr />
+              <section className='flex'>
+              <img className='mt-5' src={place.imageUrls[0]} alt={place.name} width="600" height="600" />
+              <p className='p-20 text-2xl'>{place.description}</p>
+              {/* <p> {place.category.name}</p> */}
+              </section>
+              <section className='flex mt-15'>
+              <p className='pt-20 text-2xl'>{place.description}</p>
+              <img src={place.imageUrls[1]} alt={place.name} width="600" height="600" />
+              {/* <p> {place.category.name}</p> */}
+              </section >
+              <section className='flex gap-6 mt-30'>
+              <img src={place.imageUrls[2]} alt={place.name} width="600" height="600" />
+              <img src={place.imageUrls[3]} alt={place.name} width="600" height="600" />
+              <img src={place.imageUrls[4]} alt={place.name} width="600" height="600" />
+              </section>
+              <section className='mt-30'>
+              <h2 className='text-black font-extrabold text-3xl mb-3 ml-3.5'>{place.name}</h2>
+              <hr />
+              <p className=' mt-5 mb-5 text-2xl '>{place.description}</p>
 
-        category.map((item, index) => {
-          return (
-            <div className='text-zinc-950' key={index}>
-              <h1 className='text-xl font-bold tex-rud-500'>{item.name}</h1>
-              <p className='text-pink-300'>{item.description}</p>
-            </div>
-          )
-        })
-      }
+              </section>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+      
     </div>
   )
 }
