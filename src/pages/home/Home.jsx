3b9a2b77e77904d356a2cd930189fd840d78bf39
reanchanrin12 <ooppa5396@gmail.com>
2025-04-02 12:54:06 +0700

    home page banner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import getData from '../../services/get/getData';
+import Hero from '../../components/layouts/banner/Hero';
+import { CardPlace } from '../../components/places/CardPlace';
 
 const Home = () => {
   const [places, setPlaces] = useState([]);
-
   useEffect(() => {
     async function getCategories() {
-      const data = await getData("places"); 
-      console.log("Fetched Data:", data); // ✅ Debug API response
-
+      const data = await getData("places");
+      console.log("Fetched Data:", data);
       if (Array.isArray(data)) {
-        setPlaces(data); // ✅ Use the array directly
+        setPlaces(data);
       } else {
         console.error("Error: API responsed is not an array", data);
       }
@@ -19,22 +19,27 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Home</h1>
-      {places.length > 0 ? (
-        <ul>
-          {places.map((place) => (
-            <li key={place.id}>
-              <h2>{place.name}</h2>
-              <p>{place.description}</p>
-              <p><strong>Category:</strong> {place.category.name}</p>
-              <img src={place.imageUrls[0]} alt={place.name} width="200" />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div className="mx-auto max-w-screen-2xl px-4 py-8 font-[Suwannaphum]">
+      <div className='mb-10'>
+        <Hero />
+      </div>
+      <section className='mb-8'>
+        <CardPlace />
+        {places.length > 0 ? (
+          <ul className='mb-5'>
+            {places.map((place) => (
+              <li key={place.id}>
+                <h2>{place.name}</h2>
+                <p>{place.description}</p>
+                <p><strong>Category:</strong> {place.category.name}</p>
+                <img src={place.imageUrls[0]} alt={place.name} width="200" />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
     </div>
   );
 }
