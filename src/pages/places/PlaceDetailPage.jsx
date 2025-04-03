e4e6575a8f4ccc9edc85f25466c8e5bb227f0be6
khakha <khimkhunkha@gmail.com>
2025-04-03 16:13:47 -0600

    some changes in the detail

--- src/pages/places/PlaceDetailPage.jsx
+++ src/pages/places/PlaceDetailPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import getData from '../../services/get/getData';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import getData from "../../services/get/getData";
 
 const PlaceDetailPage = () => {
   const [place, setPlace] = useState(null);
@@ -13,7 +13,7 @@ const PlaceDetailPage = () => {
         console.log("Fetched place data:", data);
         setPlace(data);
       } catch (error) {
-        console.error('Error fetching place data:', error);
+        console.error("Error fetching place data:", error);
       }
     }
 
@@ -25,32 +25,46 @@ const PlaceDetailPage = () => {
   }
 
   return (
-    <div>
-      <h2 className="text-black font-extrabold text-5xl mb-6 ml-3.5 mt-10">{place.name}</h2>
+    <article className="px-[8%]">
+      <h2 className="text-black font-extrabold text-5xl mb-6 ml-3.5 mt-10">
+        {place.name}
+      </h2>
       <hr />
-
-      <section className="flex">
-        <img className="mt-5" src={place.imageUrls?.[0]} alt={place.name} width="600" height="600" />
-        <p className="p-20 text-2xl">{place.description}</p>
+      <section className="grid grid-cols-2 md:grid-cols-4 gap-5 auto-rows-[200px] md:auto-rows-[250px]">
+        <img
+          className="h-auto w-auto object-cover col-span-4 "
+          src={place.imageUrls?.[0]}
+          alt={place.name}
+        />
+        <img
+          className="mt-40 h-auto w-auto object-cover col-span-1 "
+          src={place.imageUrls?.[1]}
+          alt={place.name}
+        />
+        <img
+          className="mt-40 h-auto w-auto object-cover col-span-1 "
+          src={place.imageUrls?.[2]}
+          alt={place.name}
+        />
+        <img
+          className="mt-40 h-auto w-auto object-cover col-span-1 "
+          src={place.imageUrls?.[3]}
+          alt={place.name}
+        />
+        <img
+          className="mt-40 h-auto w-auto object-cover col-span-1 "
+          src={place.imageUrls?.[4]}
+          alt={place.name}
+        />
       </section>
-
-      <section className="flex mt-15">
+      <section>
+        <h2 className="text-black font-extrabold text-5xl mb-6 ml-3.5 mt-30">
+          {place.name}
+        </h2>
         <p className="pt-20 text-2xl">{place.description}</p>
-        <img src={place.imageUrls?.[1]} alt={place.name} width="600" height="600" />
-      </section>
-
-      <section className="flex gap-6 mt-30">
-        {place.imageUrls?.slice(2, 5).map((url, index) => (
-          <img key={index} src={url} alt={`${place.name} ${index + 3}`} width="600" height="600" />
-        ))}
-      </section>
-
-      <section className="mt-30">
-        <h2 className="text-black font-extrabold text-3xl mb-3 ml-3.5">{place.name}</h2>
         <hr />
-        <p className="mt-5 mb-5 text-2xl">{place.description}</p>
       </section>
-    </div>
+    </article>
   );
 };
 
---
+++ src/pages/places/page-conserve.jsx
@@ -0,0 +1,55 @@
+
+
+const PlaceDetailPage = () => {
+  const [place, setPlace] = useState(null);
+  const { uuid } = useParams();
+
+  useEffect(() => {
+    async function fetchPlace() {
+      try {
+        const data = await getData(`places/${uuid}`);
+        console.log("Fetched place data:", data);
+        setPlace(data);
+      } catch (error) {
+        console.error('Error fetching place data:', error);
+      }
+    }
+
+    fetchPlace();
+  }, [uuid]);
+
+  if (!place) {
+    return <div className="text-center mt-10 text-xl">Loading...</div>;
+  }
+
+  return (
+    <article className='px-[8%]'>
+      <h2 className="text-black font-extrabold text-5xl mb-6 ml-3.5 mt-10">{place.name}</h2>
+      <hr />
+
+      <section className="flex">
+        <img className="mt-5 h-48 w-96 object-fill " src={place.imageUrls?.[0]} alt={place.name} />
+       
+      </section>
+
+      <section className="flex mt-15">
+        <p className="pt-20 text-2xl">{place.description}</p>
+        <img src={place.imageUrls?.[1]} alt={place.name} width="600" height="600" />
+      </section>
+
+      <section className="flex gap-6 mt-30">
+        {place.imageUrls?.slice(2, 5).map((url, index) => (
+          <img key={index} src={url} alt={`${place.name} ${index + 3}`} width="600" height="600" />
+        ))}
+      </section>
+
+      <section className="mt-30">
+        <h2 className="text-black font-extrabold text-3xl mb-3 ml-3.5">{place.name}</h2>
+        <hr />
+        <p className="mt-5 mb-5 text-2xl">{place.description}</p>
+      </section>
+    </article>
+  );
+};
+
+export default PlaceDetailPage;
